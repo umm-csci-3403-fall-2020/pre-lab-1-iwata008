a9d4b241fa19ec7bd159b0b8fe1003df9ded5c43
Machi Iwata <iwata008@morris.umn.edu>
2020-09-02 00:09:46 -0500

    Still working on r2

--- regex/regex.sh
+++ regex/regex.sh
@@ -3,3 +3,5 @@
 awk 'match($0, /([a-zA-Z]+), ([a-zA-Z]+)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < r0_input.txt | tee r0_output.txt
 
 awk 'match($0, /(\w+). My favorite sandwich is (\w+)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < r1_input.txt | tee r1_output.txt
+
+awk 'match($0, /sandwich with (\w+)*. (\w+)/ groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < r2_input.txt | tee r2_output.txt
